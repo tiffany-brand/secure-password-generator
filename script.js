1a04eb6b099e70988cd9569a6436a9c69129a68a
Tiffany Brand <brand2learn@gmail.com>
2020-06-30 19:46:44 -0400

    Add comments, remove console.logs

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-// Assignment Code
+// Select generate password button
 const generateBtn = document.querySelector('#generate');
+
+// Select password parameters form elements
 const lengthBox = document.querySelector('#length');
 const lowerBox = document.querySelector('#lowercase');
 const upperBox = document.querySelector('#uppercase');
@@ -10,58 +12,60 @@ const specialCharBox = document.querySelector('#special-char');
 function writePassword() {
 	let password = generatePassword();
 	let passwordText = document.querySelector('#password');
-
 	passwordText.value = password;
 }
 
+// Logic to randomly generate password based on parameters chosen
 function generatePassword() {
+
+	// Create arrays to hold password parameters elements
 	const lowerLetters = 'abcdefghijklmnopqrstuvwxyz'.split('');
 	const upperLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
 	const numbers = '0123456789'.split('');
 	const specialChars = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~".split('');
 
+	// Initialize array for possible password characters
 	let passChoices = [];
+
+	// Initialize array for generated password
 	let passBuilder = [];
 
+	// Get length from form
 	let pswdLength = parseInt(lengthBox.value);
 
+	// Add possible password characters to array of choices based on checkboxes
 	if (lowerBox.checked) {
 		passChoices = passChoices.concat(lowerLetters);
 	}
-
 	if (upperBox.checked) {
 		passChoices = passChoices.concat(upperLetters);
 	}
-
 	if (numbersBox.checked) {
 		passChoices = passChoices.concat(numbers);
 	}
-
 	if (specialCharBox.checked) {
 		passChoices = passChoices.concat(specialChars);
 	}
 
-	console.log(passChoices);
 
+	// Validate that a length of appropriate size was input
 	if (passChoices.length) {
-		console.log(pswdLength);
-
 		if (pswdLength < 8 || pswdLength > 128 || isNaN(pswdLength)) {
 			alert('Please enter a password length between 8 and 128 characters.');
 			return '';
 		} else {
+			// Choose random characters to add to password
 			for (let i = 0; i < pswdLength; i++) {
 				let randChar = Math.floor(Math.random() * passChoices.length);
-				console.log(randChar);
 				passBuilder.push(passChoices[randChar]);
 			}
 
+			// Create string of final password and return it
 			let password = passBuilder.join('');
-			console.log(password);
-
 			return password;
 		}
 	} else {
+		// Make sure at least one parameter is checked
 		alert('Please check at least one character type.');
 		return '';
 	}
